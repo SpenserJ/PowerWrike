1790b53a0481befe94dd16cc98a82fddc1899568
Spenser Jones <spenser.e.jones@gmail.com>
2014-05-23 12:40:11 -0600

    Tweaking last patch to ensure we don't overwrite record data

--- js/menu_status.js
+++ js/menu_status.js
@@ -26,8 +26,11 @@ define(['js/debug', 'js/dropdown', 'js/statuses', 'js/task', 'js/events'], funct
     // Return if we're not focused on a task, or are getting an update for a different task
     if (currentTask === false || (typeof record !== 'undefined' && currentTask.id !== record.id)) { return; }
 
-    // If we're using an updated record, ensure the data property is set
-    if (typeof record !== 'undefined') { currentTask = record; currentTask.data = record; }
+    if (typeof record !== 'undefined') {
+      currentTask = record;
+      // If we're using an updated record, ensure the data property is set
+      if (typeof currentTask.data === 'undefined') { currentTask.data = record; }
+    }
 
     // If we have a current task, but it isn't fully loaded, try again in 100ms
     if (typeof currentTask.data === 'undefined' || typeof currentTask.data.parentFolders === 'undefined') {
