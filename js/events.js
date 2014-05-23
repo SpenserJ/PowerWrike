a0f99694323681bacfd363744448de13bf5b5a80
Spenser Jones <spenser.e.jones@gmail.com>
2014-05-23 12:00:47 -0600

    Detect new mentions while the page is running, and display a facebook-esque indicator of how many unread there are

--- js/events.js
+++ js/events.js
@@ -18,5 +18,17 @@ define(['js/debug', 'lib/EventEmitter'], function (debug, EventEmitter) {
   // Delay the event for overlays slightly, so it can finish loading
   $wrike.bus.on('overlay.shown', function(task) { setTimeout(function() { taskSelected(task); }, 50); });
 
+  function commentAdded(comment) {
+    ee.emitEvent('task.comment', [comment]);
+  }
+
+  $wrike.bus.on("notifier.EventCommentAdded", commentAdded);
+
+  function notificationShown(notification) {
+    ee.emitEvent('notification.shown', notification);
+  }
+ 
+  $wrike.bus.on('desktop.notification.open.item', notificationShown);
+
   return ee;
 });
--- js/mentions.js
+++ js/mentions.js
@@ -1,8 +1,10 @@
-define(['js/debug'], function (debug) {
+define(['js/debug', 'js/events', 'js/styles'], function (debug, events, styles) {
   var mentions = []
+    , processedMentions = []
     , $mentionsPanel
     , $mentionsDropdown
-    , mentionsCount = { total: 0, unread: 0 };
+    , mentionsCount = { total: 0, unread: 0 }
+    , firstRun = true;
 
   function getStream() {
     var api = new Ext.data.HttpProxy({ url:'/ui/as_append3' });
@@ -26,6 +28,30 @@ define(['js/debug'], function (debug) {
     }, { read: handleMentionStreamData }, function(){});
   }
 
+  function getComments(taskId, accountId) {
+    var api = new Ext.data.HttpProxy({ url:'/ui/as_get_stream_for_entity' });
+    api.doRequest('read', null, {
+      loadDepthLimit: 5,
+      onlyAfterTimeLastRead: false,
+      taskId: taskId,
+      accountId: accountId,
+      parentFolderId: null,
+      aggregateByDays: true,
+      aggregatedRescheduleChanges: true,
+      massActionsEnabled: true,
+      loadLimit: 10,
+      noPeep: true,
+      scanLimit: 200,
+      markAsRead: false,
+      filterAuthorMe: false,
+      filterByDeltaTypes: "[comment]",
+      filterEditedByMe: false,
+      filterFollowedByMe: true,
+      filterResponsibleMe: false,
+      onlyUnread: false,
+    }, { read: handleMentionStreamData }, function(){});
+  }
+
   function handleMentionStreamData(response) {
     if (response.status !== 200) {
       debug.error('Failed to get stream data', arguments);
@@ -37,11 +63,14 @@ define(['js/debug'], function (debug) {
       , userId = $wrike.user.getUid()
       , yourMentions = [];
 
-
     // Loop through each update and each comment, looking for the current user being mentioned
     $(data.data.stream).each(function (streamIndex, streamUpdate) {
       $(streamUpdate.agentries).each(function (agentriesIndex, comment) {
         if (comment.comment.text.indexOf('<a class="stream-user-id avatar" rel="' + userId + '">') !== -1) {
+          // Skip comments that we've already processed
+          if ($.inArray(comment.id, processedMentions) !== -1) { return; }
+
+          processedMentions.push(comment.id);
           var task = streamUpdate;
           delete task.agentries;
           yourMentions.push({ task: task, comment: comment });
@@ -51,8 +80,13 @@ define(['js/debug'], function (debug) {
       });
     });
 
-    //mentions = mentions.concat(yourMentions);
-    mentions = yourMentions;
+    if (yourMentions.length === 0) { return { success: function(){} }; }
+
+    // If this is not our first run, emit an event for any new mentions
+    if (firstRun === true) { firstRun = false; }
+    else { events.emitEvent('mention', yourMentions); }
+
+    mentions = mentions.concat(yourMentions);
     // Sort mentions in reverse chronological order
     mentions.sort(function (a, b) { return b.comment.timepoint - a.comment.timepoint; });
     renderMentions();
@@ -61,12 +95,21 @@ define(['js/debug'], function (debug) {
     return { success: function(){} };
   }
 
+  function commentAdded(comment) {
+    var tasksProcessed = [];
+    $(comment.ids).each(function (i, taskId) {
+      if ($.inArray(taskId, tasksProcessed) !== -1) { return; }
+      tasksProcessed.push(taskId);
+      getComments(taskId, comment.accountId);
+    });
+  }
+  events.addListener('task.comment', commentAdded);
+
   function renderPanel() {
-    /*jshint multistr: true */
-    $mentionsPanel = $('\
+    $mentionsPanel = $(/*jshint multistr: true */ '\
 <div class="x-panel wspace-dashboard-card wrike-size-small x-portlet" style="margin-bottom: 0; top: 45px; position: fixed; bottom: 27px; right: 9px; width: 512px;">\
   <div class="x-panel-header x-unselectable wrike-panel-header" style="cursor: move;">\
-    <span class="wrike-panel-title">Mentions - Not updated after first load!</span>\
+    <span class="wrike-panel-title">Mentions - Can\'t click links yet. Find the task yourself!</span>\
   </div>\
   <div style="overflow: auto; position: absolute; bottom: 0; top: 39px;">\
     <div class="x-panel-body">\
@@ -78,10 +121,6 @@ define(['js/debug'], function (debug) {
 </div>\
     ').hide();
     $('body').append($mentionsPanel);
-
-    $mentionsDropdown = $('<span class="wspace_header_mentions wspace_header_userLink hasmenu x-btn-noicon" style="width: auto;"><span class="mentions">Mentions (???)</span></span>');
-    $mentionsDropdown.click(function () { $mentionsPanel.toggle(); });
-    $('.wrike_header_links').prepend($mentionsDropdown);
   }
 
   function renderMentions() {
@@ -95,10 +134,15 @@ define(['js/debug'], function (debug) {
         taskAuthor: $wrike.user(mention.task.uid).data(),
         firstResponsible: $wrike.user(mention.task.responsibles[0]).data(),
         timestamp: new Date(mention.comment.timepoint),
-        taskStart: new Date(mention.task.started),
-        taskEnd: new Date(mention.task.finished),
       };
-      data.taskLength = Math.ceil((data.taskEnd - data.taskStart) / 86400 / 1000);
+      if (mention.task.started === null || mention.task.finished === null) {
+        data.taskStart = data.taskEnd = null;
+        data.taskLength = 0;
+      } else {
+        data.taskStart = new Date(mention.task.started);
+        data.taskEnd = new Date(mention.task.finished);
+        data.taskLength = Math.ceil((data.taskEnd - data.taskStart) / 86400 / 1000);
+      }
 
       // If this is the same task as the last one, group the comments together
       if (typeof lastMention !== 'undefined' && lastMention.task.id === mention.task.id) {
@@ -106,8 +150,7 @@ define(['js/debug'], function (debug) {
         return;
       }
 
-      /*jshint multistr: true */
-      $mention = $('\
+      $mention = $(/*jshint multistr: true */ '\
 <div class="stream-entry stream-task-entry">\
   <div class="body body-root">\
     <div class="visual">\
@@ -130,7 +173,7 @@ define(['js/debug'], function (debug) {
       </p>\
       <p class="root-info">\
         <span>\
-          <span>Planned for ' + data.taskStart.toLocaleDateString() + ' – ' + data.taskEnd.toLocaleDateString() + ' (' + data.taskLength + 'd)</span>\
+          <span>' + ((data.taskLength !== 0) ? ('Planned for ' + data.taskStart.toLocaleDateString() + ' – ' + data.taskEnd.toLocaleDateString() + ' (' + data.taskLength + 'd)') : 'Backlogged') + '</span>\
           <span class="importance importance-' + mention.task.priority + '"><span class="icon" unselectable="on">&nbsp;&nbsp;&nbsp;</span></span>\
         </span>\
       </p>\
@@ -143,6 +186,10 @@ define(['js/debug'], function (debug) {
       var folders = [];
       $(mention.task.parentFolders).each(function (i, folderId) {
         var folder = $w.folders.getById(folderId);
+
+        // Skip any folders that we don't have access to
+        if (typeof folder === 'undefined') { return; }
+
         folders.push('<a rel="folder' + folder.id + '" ext:qmaxwidth="500" ext:qtip="folder:{id:' + folder.id + '}" ext:qparser="folder" ext:anchor="bottom" ext:qtooltip="true" class="title top-folder folder-link">' + folder.data.title + '</a>');
       });
       $mention.find('.details .parent-folders').append(folders.join(', ')).append('<span class="sqb">]</span>');
@@ -150,12 +197,11 @@ define(['js/debug'], function (debug) {
       lastMention = mention;
     });
 
-    $mentionsDropdown.find('.mentions').html('Mentions (' + mentionsCount.unread + '/' + mentionsCount.total + ')');
+    renderMentionsDropdown();
   }
 
   function renderComment(comment, data) {
-    /*jshint multistr: true */
-    var commentHtml = '\
+    var commentHtml = /*jshint multistr: true */ '\
 <div class="comment comment-firstinday  comment-firstbyuser  comment-text ">\
   <div class="date-container">\
     <span class="date date-to-be-updated date-notime" value="' + comment.timepoint + '">' + data.timestamp.toLocaleDateString() + ' at ' + data.timestamp.toLocaleTimeString() + '</span>\
@@ -183,10 +229,51 @@ define(['js/debug'], function (debug) {
     return commentHtml;
   }
 
+  function renderMentionsDropdown() {
+    var mentionsHtml = 'Mentions (' + mentionsCount.total + ') ' + 
+          '<div class="count' + ((mentionsCount.unread === 0) ? ' hidden' : '') + '">' + mentionsCount.unread + '</div>';
+    if (typeof $mentionsDropdown === 'undefined') {
+      $mentionsDropdown = $(/*jshint multistr: true */ '\
+<span class="wspace_header_mentions wspace_header_userLink hasmenu x-btn-noicon" style="width: auto;">\
+  <span class="mentions">' + mentionsHtml + '</span>\
+</span>\
+      ');
+
+      $mentionsDropdown.click(function () {
+        $mentionsPanel.toggle();
+        mentionsCount.unread = 0;
+        renderMentionsDropdown();
+      });
+
+      $('.wrike_header_links').prepend($mentionsDropdown);
+    } else {
+      $mentionsDropdown.find('.mentions').html(mentionsHtml);
+    }
+  }
+
+  styles.addStyle('mentions', /*jshint multistr: true */ '\
+    .wspace_header_mentions .count {\
+      display: inline-block;\
+      background: red;\
+      line-height: 16px;\
+      background-color: #dc0d17;\
+      background-image: -webkit-gradient(linear, center top, center bottom, from(#fa3c45), to(#dc0d17));\
+      background-image: -webkit-linear-gradient(#fa3c45, #dc0d17);\
+      color: #fff;\
+      min-height: 13px;\
+      padding: 1px 5px;\
+      text-shadow: 0 -1px 0 rgba(0, 0, 0, .4);\
+      -webkit-border-radius: 2px;\
+      -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .7);\
+      margin-left: 8px;\
+    }\
+    .wspace_header_mentions .count.hidden { display: none; }\
+  ');
+
   renderPanel();
   getStream();
 
   return {
-    getStream: getStream,
+    mentions: mentions,
   };
 });
